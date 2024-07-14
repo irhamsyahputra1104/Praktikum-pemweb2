37c2c6c436b1b3aed23c0dc07a3d2970afd231b4
irhamsyahputra1104 <irhamsyahputra1104@gmail.com>
2024-07-14 23:17:22 +0700

    upload praktikum

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@php
+    $nama = 'Irham Syahputra';
+    $nim = '0110223244';
+    $prodi = 'Teknik Informatika';
+    $angkatan = '2023';
+@endphp
+
+<p>{{ $nama }}</p>
+<p>{{ $nim }}</p>
+<p>{{ $prodi }}</p>
+<p>{{ $angkatan }}</p>
